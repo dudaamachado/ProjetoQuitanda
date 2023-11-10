5d1184b82f859aa91772a8ea399091f36915caed
dudaamachado <maria.machado.senai@gmail.com>
2023-11-10 09:59:48 -0300

    Colocando a página de adicionar itens e cadastro.

--- quitanda.py
+++ quitanda.py
@@ -46,6 +46,12 @@ def login():
     title="Login"
     return render_template("login.html", title=title)
 
+#ROTA DA PÁGINA LOGIN 
+@app.route("/login")
+def login():
+    title="Login"
+    return render_template("login.html",title=title)
+
 # ROTA DA PÁGINA ACESSO
 @app.route("/acesso", methods=['post'])
 def acesso():
@@ -60,5 +66,53 @@ def acesso():
 
 render_template("login.html",msg="Usuário/Senha estão incorretos!")
 
+#ROTA DE PÁGINA ADM 
+@app.route("/adm")
+def adm():
+    if verifica_sessao():
+        iniciar_db()
+        conexao = conecta_database()
+        produtos = conexao.execute('SELECT * FROM produtos ORDER BY id_prod DESC').fetchall()
+        conexao.close
+        title = "Administração"
+        return render_template("adm.html", produtos=produtos, title=title)
+    else:
+        return redirect("/login")
+    
+#CÓDIGO DE LOGOUT para que o administrador possa sinalizar que já utilizou o sistema e irá sair do modo administrativo.
+@app.route("/logout")
+def logout():
+    global login 
+    login = False 
+    session.clear()
+    return redirect('/')
+
+#ROTA DA PÁGINA DE CADASTRO
+@app.route("/cadprodutos")
+def cadprodutos():
+    if verifica_sessao():
+        title = "Cadastro de produtos"
+        return render_template("cadprodutos.html", title=title)
+    else:
+        return redirect("/login")
+    
+#ROTA DA PÁGINA DE CADASTRO NO BANCO
+@app.route("/cadastro",methods=["post"])
+def cadastro():
+    if verifica_sessao():
+        nome_prod=request.form['nome_prod']
+        desc_prod=request.form['desc_prod']
+        preco_prod=request.form['preco_prod']
+        img_prod=request.form['img_prod']
+        id_foto=srt(uuid.uuid4().hex)
+        filename=id_foto+nome+prod+'.png'
+        img_prod.save("static/img/produtos"+filename)
+        conexao = conecta_database()
+        conexao.execute('INSERT INTO produtos (nome_prod, desc_prod, preco_prod, img_prod) VALUE (?, ?, ?, ?)',(nome_prod, desc_prod, preco_prod, filename))
+        conexao.commit()
+        conexao.close()
+        return redirect("/adm")
+    else:
+        return redirect("/login")
 # FINAL DO CODIGO - EXECUTANDO O SERVIDOR
 app.run(debug=True)
